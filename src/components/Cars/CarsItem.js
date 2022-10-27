202ea6273fbac32a9fcb7e7aba5c867fd2a283f3
Mohi Uddin Mahim <mohi35-2790@diu.edu.bd>
2022-10-27 16:19:44 +0600

    padding fixed on cars item

--- src/components/Cars/CarsItem.js
+++ src/components/Cars/CarsItem.js
@@ -3,7 +3,7 @@ import React from "react";
 const CarsItem = ({ car }) => {
   const { name, cars, color, city } = car;
   return (
-    <div className="border border-slate-400 p-5 cursor-pointer hover:bg-amber-400 hover:text-white">
+    <div className="border border-slate-400 md:p-5 p-2 cursor-pointer hover:bg-amber-400 hover:text-white">
       <h4 className="font-medium">{name}</h4>
       <p>{`Cars: ${cars.join(",")}`}</p>
       <p>{`Colors: ${color.join(",")}`}</p>
